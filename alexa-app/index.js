9b45bc7439c781533e609d85705ed39eed451d08
Timo Loewe <timoloewe91@gmail.com>
2016-11-19 23:13:14 +0100

    Case 2

--- alexa-app/index.js
+++ alexa-app/index.js
@@ -1,9 +1,23 @@
 var alexa = require('alexa-app');
 var get = require('request');
-var dateformat = require('dateformat');
+var dateFormat = require('dateformat');
 var app = new alexa.app('sample');
 
-app.intent('QuickBook',
+function formatDate(dateIso) {
+    return dateFormat(new Date(dateIso), "dddd, mmmm dS, h:MM TT");
+}
+
+function formatDateShort(dateIso) {
+    return dateFormat(new Date(dateIso), "dddd, h:MM TT");
+}
+
+function buildParams(offer) {
+    return "?pickupLocation=" + offer.city +
+        "&pickupDate=" + offer.pickupDate +
+        "&returnDate=" + offer.returnDate;
+}
+
+app.intent('NewBooking',
     {
         "slots": {
             "city": "AMAZON.DE_CITY",
@@ -15,8 +29,8 @@ app.intent('QuickBook',
         , "utterances": ["book a car in {-|city} from {-|pickupDate} {-|pickupTime} to {-|returnDate} {-|returnTime}"]
     },
 
-    function (request, response) {
-        console.log("QuickBookIntent");
+    function(request, response) {
+        console.log("NewBooking");
 
         var city = request.slot('city');
         var pickupDate = request.slot('pickupDate');
@@ -24,26 +38,22 @@ app.intent('QuickBook',
         var returnDate = request.slot('returnDate');
         var returnTime = request.slot('returnTime');
 
-        var params = "?pickupLocation=" + city +
+        get("https://sixt-bot.herokuapp.com/getoffers?pickupLocation=" + city +
             "&pickupDate=" + pickupDate + "T" + pickupTime +
             "&returnDate=" + returnDate + "T" + returnTime +
-            "&status=new";
-
-        get("https://sixt-bot.herokuapp.com/getoffers" + params,
-            function (error, resp, body) {
+            "&status=new",
+            function(error, resp, body) {
                 var offers = JSON.parse(body);
                 var offer = offers[0];
 
                 console.log(offers);
 
-                offer.pickupDate = dateformat(new Date(offer.pickupDate), "dddd, mmmm dS, h:mm TT");
-                offer.returnDate = dateformat(new Date(offer.returnDate), "dddd, mmmm dS, h:mm TT");
+                response.session("case", 1);
+                response.session("params", buildParams(offer));
 
-                response.session("case1", params);
-
-                response.say("You want to book a car in " + offer.pickupLocation + 
-                    " from " + offer.pickupDate +
-                    " to " + offer.returnDate + ".")
+                response.say("You want to book a car in " + offer.pickupLocation +
+                    " from " + formatDate(offer.pickupDate) +
+                    " to " + formatDate(offer.returnDate) + ".")
                     .say("I can offer you a " + offer.carExample + " or similar for " + offer.price + " Euro.")
                     .say("Should I book it now?")
                     .shouldEndSession(false)
@@ -56,17 +66,48 @@ app.intent('QuickBook',
 
 app.intent('AMAZON.YesIntent',
 
-    function (request, response) {
+    function(request, response) {
         console.log("YesIntent");
 
-        get("https://sixt-bot.herokuapp.com/confirm" + request.session("case1"),
-            function (error, resp, body) {
+        get("https://sixt-bot.herokuapp.com/confirm" + request.session("params"),
+            function(error, resp, body) {
                 response.say("I confirm. The car is booked.").send();
             });
 
         return false;
     }
 );
+
+app.intent("UsualBooking",
+    {
+        "utterances": ["book a car"]
+    },
+
+    function(request, response) {
+        console.log("UsualBookingIntent");
+
+        get("https://sixt-bot.herokuapp.com/getoffers?status=usual",
+            function(error, resp, body) {
+                var offers = JSON.parse(body);
+                var offer = offers[0];
+
+                console.log(offers);
+
+                response.session("case", 2);
+                response.session("params", buildParams(offer));
+
+                response.say("Ok, I'm looking for your usual request in " + offer.pickupLocation +
+                    " from " + formatDateShort(offer.pickupDate) +
+                    " to " + formatDateShort(offer.returnDate) + ".")
+                    .say("I can offer you a " + offer.carExample + " or similar for " + offer.price + " Euro.")
+                    .say("Should I book it now?")
+                    .shouldEndSession(false)
+                    .send();
+            });
+
+        return false;
+    }
+)
 
 console.log(app.schema());
 console.log(app.utterances());
