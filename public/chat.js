d2eac5c3d5c79b94397fd8d9925d38a1a44fd471
UBSURFACEPRO3\UBderLMU <info@maxmediapictures.de>
2016-11-19 20:39:52 +0100

    fixed chat sytle

--- public/chat.js
+++ public/chat.js
@@ -1,35 +1,35 @@
 var socket = io();
 
 socket.on('chat', function (msg) {
-    var print_date = msg.time.getHours() + ':' + msg.time.getMinutes() + ', ' + msg.time.getDay() + '.' + msg.time.getMonth();
+    console.log(msg);
 
-    if (sender == "bot") {
+    if (msg.sender == "user") {
+        msg.time = new Date(msg.time);
+        var print_date = msg.time.getHours() + ':' + msg.time.getMinutes() + ', ' + msg.time.getDay() + '.' + msg.time.getMonth();
+
         var html = '<li class = "clearfix" >' +
             '   <div class = "message-data align-right" >' +
             '   <span class = "message-data-time" > ' + print_date + ' </span> &nbsp; &nbsp; ' +
             '   <span class = "message-data-name" > Max Mustermann </span> <i class="fa fa-circle me"></i >' +
             '</div>' +
-            '<div class="message other-message float-right">' + msg.text + '</div>';
+            '<div class="message other-message float-right">' + msg.text + '</div></li>';
 
         $('#messages').append(html);
 
 
-    } else {
-        var html = '<li class = "clearfix" >' +
+    } else if (msg.sender == "bot") {
+        msg.time = new Date(msg.time);
+        var print_date = msg.time.getHours() + ':' + msg.time.getMinutes() + ', ' + msg.time.getDay() + '.' + msg.time.getMonth();
+
+        var html = '<li >' +
             '   <div class = "message-data" >' +
+            '   <span class = "message-data-name" > Sixt Bot </span> <i class="fa fa-circle online"></i >' +
             '   <span class = "message-data-time" > ' + print_date + ' </span> &nbsp; &nbsp; ' +
-            '   <span class = "message-data-name" > Max Mustermann </span> <i class="fa fa-circle me"></i >' +
+
             '</div>' +
-            '<div class="message my-message">' + msg.text + '</div>';
+            '<div class="message my-message">' + msg.text + '</div></li>';
 
         $('#messages').append(html);
     }
 
 });
-
-
-< div class = "message-data" >
-
-
-
-
