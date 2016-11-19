de9f4a8688d0d9ac6d62d6b504226dd6e6fe082c
Tibor Goldschwendt <tibor.goldschwendt@googlemail.com>
2016-11-19 15:25:38 +0100

    Can handle addresses.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 app.get('/bookcar', function(req, res) {
 
-	bookCar('11', new Date(2016, 11, 20), new Date(2016, 11, 23), function(offer) {
+	bookCar('Muenchen', new Date(2016, 11, 20, 8), new Date(2016, 11, 23, 8), function(offer) {
      	res.setHeader('Content-Type', 'application/json');
       	res.send(JSON.stringify(offer, null, 3));
   	});
@@ -23,21 +23,32 @@ app.get('/bookcar', function(req, res) {
 
 function bookCar(pickup_location, pickup_date, return_date, callback) {
 
-	request('https://app.sixt.de/php/mobilews/v4/offerlist?pickupStation=' + pickup_location + '&returnStation=' + pickup_location + '&pickupDate=' + pickup_date.toISOString() + '&returnDate=' + return_date.toISOString(), function(error, resp, body) {
-		body = JSON.parse(body);
+	// Request location.
+	// TODO: take closest location.
+	request('https://app.sixt.de/php/mobilews/v4/stationsuggestion?address=' + pickup_location, function(error, resp, bodyLocation) {
+		bodyLocation = JSON.parse(bodyLocation);
 
+		var pickupLocationId = bodyLocation.downtownStations[0].identifier;
 
+		// Request offer.
+		// TODO: flexi price.
+		// TODO: cheapest offer.
+		request('https://app.sixt.de/php/mobilews/v4/offerlist?pickupStation=' + pickupLocationId + '&returnStation=' + pickupLocationId + '&pickupDate=' + pickup_date.toISOString() + '&returnDate=' + return_date.toISOString(), function(error, resp, bodyOffer) {
+			bodyOffer = JSON.parse(bodyOffer);
 
-		var offer = {
-    		pickup_location: pickup_location,
-    		return_location: pickup_location,
-    		pickup_date: pickup_date,
-		    return_date: return_date,
-		    price: body.offers[0].rates[0].price.totalPrice,
-		    car_exmaple: body.offers[0].group.modelExample
-		};
+			console.log(bodyOffer)
 
-		callback(offer);
+			var offer = {
+	    		pickup_location: bodyLocation.downtownStations[0].name,
+	    		return_location: bodyLocation.downtownStations[0].name,
+	    		pickup_date: pickup_date,
+			    return_date: return_date,
+			    price: bodyOffer.offers[0].rates[0].price.totalPrice,
+			    car_exmaple: bodyOffer.offers[0].group.modelExample
+			};
+
+			callback(offer);
+		});
 	});
 }
 
